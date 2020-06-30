7a27d742e0a53bd9834906daa22bb9f6dff0f6f3
Corey Ferguson <corey.t.ferguson@gmail.com>
2020-06-30 13:51:52 -0700

    firing bow takes stick

--- src/items/Bow/UseBow.gd
+++ src/items/Bow/UseBow.gd
@@ -3,11 +3,14 @@ extends Node2D
 var Arrow = preload('./Arrow.tscn')
 
 var arrow_speed = 500
+var required_resources = [ { 'name': 'stick', 'quantity': 1 } ]
 
 func use():
-	var target = get_global_mouse_position()
-	var arrow = Arrow.instance()
-	Env.add(arrow)
-	arrow.global_position = global_position - Vector2(0, 25)
-	arrow.velocity = (target - arrow.global_position).normalized() * arrow_speed
-	arrow.rotation = arrow.velocity.angle()
+	if Inventory.has_resources(required_resources):
+		Inventory.remove(required_resources)
+		var target = get_global_mouse_position()
+		var arrow = Arrow.instance()
+		Env.add(arrow)
+		arrow.global_position = global_position - Vector2(0, 25)
+		arrow.velocity = (target - arrow.global_position).normalized() * arrow_speed
+		arrow.rotation = arrow.velocity.angle()
